08152b3c3afe88559e41b7fbfa6771708908b633
Hu Yangsheng <781961324@qq.com>
2020-03-15 19:26:32 +0800

    first commit

---
+++ src/main/java/com/crud/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.crud.service;
+
+import com.crud.bean.Employee;
+import com.crud.dao.EmployeeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    EmployeeMapper employeeMapper;
+
+    /**
+     * 查询所有员工
+     * @return
+     */
+    public List<Employee> getAll(){
+        return employeeMapper.selectByExampleWithDept(null);
+    }
+}
